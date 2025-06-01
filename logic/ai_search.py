be937ba6dd28d870ce4704563b435f107beba51b
ellestianamr <ellestiana.23010@mhs.unesa.ac.id>
2025-06-01 08:40:05 +0700

    Memperbarui parameter threshold untuk mengatur batas minimum similarity

--- logic/ai_search.py
+++ logic/ai_search.py
@@ -3,21 +3,23 @@ from sentence_transformers import SentenceTransformer, util
 # Load model NLP (sekali)
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-def ai_search(recipes, user_ingredients):
+def ai_search(recipes, user_ingredients, threshold=0.5):
     user_query = ", ".join(user_ingredients)
     user_embedding = model.encode(user_query, convert_to_tensor=True)
 
     results = []
 
     for recipe in recipes:
-        recipe_ingredients_text = ", ".join(recipe["ingredients"])
-        recipe_embedding = model.encode(recipe_ingredients_text, convert_to_tensor=True)
+        recipe_text = ", ".join(recipe["ingredients"])
+        recipe_embedding = model.encode(recipe_text, convert_to_tensor=True)
 
         similarity = util.cos_sim(user_embedding, recipe_embedding)[0][0].item()
         results.append((recipe, similarity))
 
-    # Urutkan berdasarkan skor kemiripan
+    # Urutkan berdasarkan skor kemiripan (descending)
     results.sort(key=lambda x: x[1], reverse=True)
 
-    # Threshold opsional (0.5 bisa disesuaikan)
-    return [r[0] for r in results if r[1] > 0.5]
+    # Filter berdasarkan threshold minimum similarity
+    filtered_results = [(r[0], r[1]) for r in results if r[1] >= threshold]
+
+    return filtered_results
